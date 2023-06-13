22876d52aa58684aeb20b11e42ea91aeee471fcf
Yves <cauayves5@gmail.com>
2023-06-13 20:18:33 -0300

    feat: project requirements base done

--- src/pages/HISTORY/Historico.jsx
+++ src/pages/HISTORY/Historico.jsx
@@ -1,7 +1,27 @@
+import { styled } from "styled-components"
+import HeaderBar from "../../components/HeaderBar"
+import TrackBar from "../../components/TrackBar"
+import COLORS from "../../constants/colors"
 export default function History() {
     return (
-        <>
-            <p>hello history</p>
-        </>
+        <Main>
+            <HeaderBar />
+            <Content>
+                <h1>Em breve você poderá ver o histórico de hábitos aqui.</h1>
+            </Content>
+            <TrackBar />
+        </Main>
     )
 }
+const Main = styled.main`
+    height: 100vh;
+    background-color: ${COLORS.BACKGROUND};
+`
+const Content = styled.div`
+    padding: 0px 15px;
+
+    h1{
+        color: black;
+        margin: 30px 0px;
+    }
+`
